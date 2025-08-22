833fc1119a1cded8018fe053dbc1c89e69740a7f
GaganBelgur <gagan.belgur48@gmail.com>
2025-08-22 16:29:44 +0530

    Updated work experience view

---
+++ GaganBelgurPortfolio/js/experience.js
@@ -0,0 +1,10 @@
+function toggleExpand(button) {
+  const content = button.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+    button.textContent = "Show More";
+  } else {
+    content.style.display = "block";
+    button.textContent = "Show Less";
+  }
+}
